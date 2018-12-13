31cdd9fdf4a8c812efe119cae3291d60bc3650ec
Levi McCallum <levi@levimccallum.com>
2018-12-13 12:21:25 -0800

    Support running on dev hardware and clean up flask project

--- decaf.py
+++ decaf.py
@@ -1,11 +1,11 @@
-from models import BrewSettings, CoffeeInfo, barcodeScanner, relayControl, pinMappings
 from flask import Flask, request, jsonify, make_response
 from flask_restful import Resource, Api
 from flask.views import MethodView
 from flaskext.mysql import MySQL
-from decafConfig import mysql, dbuser, dbpass, dbdb, dbhost
 import simplejson as json
 
+from decafConfig import mysql, dbuser, dbpass, dbdb, dbhost
+import models
 
 app = Flask(__name__)
 api = Api(app)
@@ -18,34 +18,33 @@ mysql.init_app(app)
 
 
 #query brew settings by user id
-class brewSettings(Resource):
+class BrewSettings(Resource):
 	def get(self, userId):
-		return {'brewSettings': BrewSettings.get(userId)}
+		return {'brewSettings': models.BrewSettings.get(userId)}
 
 #get info about coffee by coffeeTypeID
-class coffeeInfo(Resource):
+class CoffeeInfo(Resource):
 	def get(self, coffeeTypeId):
-		return {'CoffeeInfo': CoffeeInfo.get(coffeeTypeId)}
+		return {'coffeeInfo': models.CoffeeInfo.get(coffeeTypeId)}
 
 #get coffee info by using the barcode scanner
-class barCode(Resource):
+class BarCode(Resource):
 	def get(self):
-		return {'barcodeScanner': barcodeScanner.get(self)}
+		return {'barcodeScanner': models.BarcodeScanner.get()}
 
-class relayController(Resource):
+class RelayController(Resource):
 	def get(self, pinNumber, relayChannel, timeOn, repeatValue, repeatDelay, connectedHardware):
-		return {'relayControl': relayControl.get(pinNumber, relayChannel, timeOn, repeatValue, repeatDelay, connectedHardware)}
+		return {'relayControl': models.RelayControl.get(pinNumber, relayChannel, timeOn, repeatValue, repeatDelay, connectedHardware)}
 
-class pinInfo(Resource):
+class PinInfo(Resource):
 	def get(self, hardwareType):
-		return {'pinMappings': pinMappings.get(hardwareType)}
+		return {'pinMappings': models.PinMappings.get(hardwareType)}
 
-api.add_resource(brewSettings, '/BrewSettings/<int:userId>')
-api.add_resource(coffeeInfo, '/CoffeeInfo/<int:coffeeTypeId>')
-api.add_resource(barCode, '/barcodeScanner/', methods=['GET'])
-api.add_resource(pinInfo, '/pinInfo/<hardwareType>')
-api.add_resource(relayController, '/relayControl/<int:pinNumber>/<int:relayChannel>/<timeOn>/<int:repeatValue>/<int:repeatDelay>/<string:connectedHardware>')
-
+api.add_resource(BrewSettings, '/brewSettings/<int:userId>')
+api.add_resource(CoffeeInfo, '/coffeeInfo/<int:coffeeTypeId>')
+api.add_resource(BarCode, '/barcodeScanner')
+api.add_resource(PinInfo, '/pinInfo/<hardwareType>')
+api.add_resource(RelayController, '/relayControl/<int:pinNumber>/<int:relayChannel>/<timeOn>/<int:repeatValue>/<int:repeatDelay>/<string:connectedHardware>')
 
 if __name__ == "__main__":
   #remove in production

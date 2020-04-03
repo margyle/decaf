ff24491fe24bfa4a57aa27e9db1c2342c4fba86c
Paul Angerer <paul.angerer@bitpanda.com>
2020-04-03 13:14:41 +0200

    added db.py argument to bootstrap data

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+import argparse
 import sqlite3
 
 from flask import g
@@ -5,12 +6,18 @@ from flask import g
 _DATABASE = "mugsy"
 
 
-def bootstrap_data(app):
-    with app.app_context():
-        db = get_db()
-        with app.open_resource("db/schema.sql", mode="r") as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+def bootstrap_data():
+    conn = None
+    try:
+        conn = sqlite3.connect(f"db/{_DATABASE}.db")
+        with open(f"db/bootstrap.sql", "r") as f:
+            conn.executescript(f.read())
+        print("Bootstrapped data.")
+    except sqlite3.Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
 
 
 def init_db(app):
@@ -49,4 +56,10 @@ def _init_db():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Create and bootstrap database.')
+    parser.add_argument("--bootstrap", action="store_true", help="Bootstrap data provided in bootstrap.sql")
+    args = parser.parse_args()
+
     _init_db()
+    if args.bootstrap:
+        bootstrap_data()
